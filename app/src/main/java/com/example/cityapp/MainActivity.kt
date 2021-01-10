ab426977badf46053d8313333ced2ce4e37cfe81
Adrian Kozłowski <adrian.kozlowski2611@interia.pl>
2021-01-10 15:37:12 +0100

    class Refactor

--- app/src/main/java/com/example/cityapp/MainActivity.kt
+++ app/src/main/java/com/example/cityapp/MainActivity.kt
@@ -13,21 +13,6 @@ import java.lang.ref.WeakReference
 
 class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
-    /*private val NAMESPACE = "http://akozlowski/soap/"
-    private val METHOD_NAME1 = "getImageResponse"
-    private val URL = "http://192.168.1.17:8080/getImageResponse"
-    private val SOAP_ACTION = "http://akozlowski/soap/getImageResponse"
-    private var webResponse = ""*/
-    /*private val NAMESPACE: String? = "http://www.w3schools.com/webservices/"
-    private val MAIN_REQUEST_URL = "https://www.w3schools.com/xml/tempconvert.asmx"
-    private val SOAP_ACTION = "http://www.w3schools.com/webservices/FahrenheitToCelsius"
-    private var METHOD_NAME = "FahrenheitToCelsius"
-    private var webResponse = ""*/
-
-    private val SOAP_ACTION = "http://www.w3schools.com/webservices/CelsiusToFahrenheit"
-    private val METHOD_NAME = "CelsiusToFahrenheit"
-    private val NAMESPACE = "http://www.w3schools.com/webservices/"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,68 +23,11 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
             val y2 = findViewById<EditText>(R.id.y2).text.toString().toInt()
             val x2 = findViewById<EditText>(R.id.x2).text.toString().toInt()*/
 //            val myRequest = myAsyncTask( WeakReference<Activity>(MainActivity()), x1, x2, y1, y2)
-            val myRequest = myAsyncTask( WeakReference<ImageView>(findViewById<ImageView>(R.id.iv_city_image)), 0, 0, 200, 200)
+            val myRequest = myAsyncTask( WeakReference<ImageView>(findViewById<ImageView>(R.id.iv_city_image)), 0, 0, 400, 400)
 
             myRequest.execute()
-            /*  val bis = ByteArrayInputStream(execute)
-              val bImage2: BufferedImage = ImageIO.read(bis)
-              ImageIO.write(bImage2, "jpg", File("output.jpg"))*/
+
 //        }
 
     }
-
-
-    /* override fun onCreate(savedInstanceState: Bundle?) {
-         super.onCreate(savedInstanceState)
-         setContentView(R.layout.activity_main)
-         val button = findViewById<Button>(R.id.btn_get_image)
-         val cityImage = findViewById<ImageView>(R.id.iv_city_image)
-         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-         StrictMode.setThreadPolicy(policy)
-
-         button.setOnClickListener {
-             *//*val y1 = findViewById<EditText>(R.id.y1).text.toString().toInt()
-            val x1 = findViewById<EditText>(R.id.x1).text.toString().toInt()
-            val y2 = findViewById<EditText>(R.id.y2).text.toString().toInt()
-            val x2 = findViewById<EditText>(R.id.x2).text.toString().toInt()
-            val url = "/getImage?y1=$y1&x1=$x1&y2=$y2&x2=$x2"*//*
-            launch(Dispatchers.Main) {
-                //        setContentView(R.layout.fragment_item_detail);
-//        textView1 = (TextView) findViewById(R.id.item_detail);
-                *//*val title = intent.extras!!.getString("ClientCode")
-
-                println("Data is $title")
-                val getReport = GetReport()
-                getReport.startWebAccess(title)*//*
-                try {
-                    val request = SoapObject(NAMESPACE, METHOD_NAME)
-
-                    val envelope = SoapSerializationEnvelope(
-                        SoapEnvelope.VER11
-                    )
-                    envelope.dotNet = true
-                    envelope.setOutputSoapObject(request)
-
-                    val androidHttpTransport = HttpTransportSE(
-                        MAIN_REQUEST_URL
-                    )
-                    androidHttpTransport.debug = true
-                    androidHttpTransport.call(SOAP_ACTION, envelope)
-                    val objectResult = envelope.bodyIn as SoapObject
-                    webResponse = objectResult.toString()
-                }catch (xe: XmlPullParserException){
-                    xe.printStackTrace()
-
-                }catch (e: Exception) {
-                    e.printStackTrace()
-                }
-
-
-               *//* RetrofitImage.getBitmapFrom(url){
-                    cityImage.load(it)
-                }*//*
-
-            }
-        }
-    }*/
 }
--- app/src/main/java/com/example/cityapp/myAsyncTask.kt
+++ app/src/main/java/com/example/cityapp/myAsyncTask.kt
@@ -1,12 +1,10 @@
 package com.example.cityapp
 
-import android.app.Activity
 import android.graphics.BitmapFactory
 import android.os.AsyncTask
 import android.util.Base64
 import android.util.Log
 import android.widget.ImageView
-import coil.api.load
 import org.ksoap2.SoapEnvelope
 import org.ksoap2.SoapFault
 import org.ksoap2.serialization.PropertyInfo
@@ -21,50 +19,18 @@ import java.lang.ref.WeakReference
 
 class myAsyncTask(val imageView: WeakReference<ImageView>, val y1: Int, val x1: Int, val y2: Int, val x2: Int) : AsyncTask<Any?, Void?, Any?>() {
 
-
-
-
-//    private val SOAP_ACTION = "http://www.w3schools.com/webservices/CelsiusToFahrenheit"
-    /*private val SOAP_ACTION = "https://www.w3schools.com/xml/CelsiusToFahrenheit"
-    private val METHOD_NAME = "CelsiusToFahrenheit"
-    private val NAMESPACE = "http://www.w3schools.com/webservices/"*/
-    /*private val SOAP_ACTION = "http://spring.io/guides/gs-producing-web-service/getCountryRequest"
-    private val METHOD_NAME = "getCountryRequest"
-    private val NAMESPACE = "http://spring.io/guides/gs-producing-web-service"*/
-private val SOAP_ACTION = "http://spring.io/guides/gs-producing-web-service/getImageRequest"
+    private val SOAP_ACTION = "http://spring.io/guides/gs-producing-web-service/getImageRequest"
     private val METHOD_NAME = "getImageRequest"
     private val NAMESPACE = "http://akozlowski/soap"
+    private val URL = "http://192.168.1.26:8080/ws"
     override fun doInBackground(vararg params: Any?): Any? {
-        val URL = "http://192.168.1.26:8080/ws"
-        val MAIN_REQUEST_URL = "https://www.w3schools.com/xml/tempconvert.asmx"
 
-        //for linear parameter
         val request = SoapObject(NAMESPACE, METHOD_NAME)
-//        request.addProperty("Celsius", "48") // adding method property here serially
-        /*val s: String = "Spain"
-        request.addProperty("name", s)*/
-       /* val GameId = PropertyInfo()
 
-        GameId.setName("name")
-        GameId.value = "Spain"
-        request.addProperty(GameId);*/
-        /*val property2 = PropertyInfo()
-        property2.name = "name"
-        property2.setName("name")
-        property2.setNamespace(NAMESPACE)
-        property2.setType(PropertyInfo.OBJECT_TYPE)
-        property2.value = nameS
-        request.addProperty(property2)*/
-       /* request.addProperty(createProperty("y1", y1))
-        request.addProperty(createProperty("x1", x1))
-        request.addProperty(createProperty("y2", y2))
-        request.addProperty(createProperty("x2", x2))*/
         request.addProperty("y1", y1)
         request.addProperty("x1", x1)
         request.addProperty("y2", y2)
         request.addProperty("x2", x2)
-
-
 
         Log.i("request", "request:" + request);
         val envelope = SoapSerializationEnvelope(SoapEnvelope.VER11)
@@ -77,15 +43,12 @@ private val SOAP_ACTION = "http://spring.io/guides/gs-producing-web-service/getI
             val requestDump = httpTransport.requestDump
             httpTransport.call(SOAP_ACTION, envelope)
         } catch (e: HttpResponseException) {
-            // TODO Auto-generated catch block
             Log.e("HTTPLOG", e.message.toString())
             e.printStackTrace()
         } catch (e: IOException) {
-            // TODO Auto-generated catch block
             Log.e("IOLOG", e.message.toString())
             e.printStackTrace()
         } catch (e: XmlPullParserException) {
-            // TODO Auto-generated catch block
             Log.e("XMLLOG", e.message.toString())
             e.printStackTrace()
         } //send request
@@ -94,29 +57,21 @@ private val SOAP_ACTION = "http://spring.io/guides/gs-producing-web-service/getI
 
 
             result = envelope.bodyIn as SoapObject
-//                result = envelope.response as SoapObject
-//            result = envelope.response as ByteArray
             Log.i("RESPONSE", result.toString()) // see output in the console
         } catch (e: SoapFault) {
-            // TODO Auto-generated catch block
             Log.e("SOAPLOG", e.message.toString())
             e.printStackTrace()
         }
-        if (result != null) {
-    val encodedImage = result.getProperty("image").toString()
-            val decodedString: ByteArray = Base64.decode(encodedImage, Base64.DEFAULT)
-            return decodedString
-        }
-        return result
+        val encodedImage = result.getProperty("image").toString()
+        val decodedString: ByteArray = Base64.decode(encodedImage, Base64.DEFAULT)
+        return decodedString
     }
 
     override fun onPostExecute(result: Any?) {
 
         val imageData: ByteArray = result as ByteArray
         val bmp = BitmapFactory.decodeByteArray(imageData, 0, imageData.size)
-        val cityImage = imageView.get()?.setImageBitmap(bmp)
-//        val cityImage = activity.get()?.findViewById<ImageView>(R.id.iv_city_image)
-//        cityImage?.load(bmp)
+        imageView.get()?.setImageBitmap(bmp)
     }
 
     fun createProperty(propertyName: String, propertyValue: Int): PropertyInfo {
